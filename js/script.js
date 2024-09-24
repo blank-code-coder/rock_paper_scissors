de3cc9ab4bea8e651e11fd6f0c88de2ffce7d053
zain <23135934+blank-code-coder@users.noreply.github.com>
2024-09-24 19:55:09 +0100

    Move script tag to body and create function
    Created function to get computer choice, output will be one of three
    strings, 'rock' 'paper' 'scissors'.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('Hello World')
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    
+    if (randomNumber === 0) {
+        return 'rock';
+    } else if (randomNumber === 1) {
+        return 'paper';
+    } else {
+        return 'scissors';
+    }
+}
+
